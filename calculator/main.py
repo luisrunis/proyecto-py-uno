89440fad55d612bdbea204371b8f04e9cca58b98
Luis Ruiz <luis.runis@hotmail.com>
2023-12-21 12:28:38 -0500

    Se realizan diferentes cambios, se cambia game por calculator, store por operaciones

---
+++ calculator/main.py
@@ -0,0 +1,10 @@
+import operaciones
+
+x = operaciones.Calcular(9,5)
+
+def run():
+  print(x.suma())
+  print(x.resta())
+
+if __name__ == '__main__':
+    run()
---
+++ calculator/operaciones.py
@@ -0,0 +1,11 @@
+class Calcular():
+ def __init__(self,x,y):
+  self.x = x
+  self.y = y
+  
+ def suma(self):
+  return self.x + self.y
+ 
+ def resta(self):
+  return self.x - self.y
+ 
